53dce8d4ec0a25725394cae4e48a7fdffa680d67
jermp96 <jjeremyperez96@gmail.com>
2025-12-29 15:36:15 -0600

    add DiscoverProvider and DiscoverScreen with MultiProvider integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,18 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tok Tik',
-      debugShowCheckedModeBanner: false,
-      theme: Apptheme().getTheme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: const Text('Hello World'),
-        ),
-      )
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DiscoverProvider())
+      ],
+      child: MaterialApp(
+        title: 'Tok Tik',
+        debugShowCheckedModeBanner: false,
+        theme: Apptheme().getTheme(),
+        home: const DiscoverScreen()
+      ),
     );
   }
 }
