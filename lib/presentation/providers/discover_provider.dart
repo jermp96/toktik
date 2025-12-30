f2b1b6e1b614379b78290b5f758d6525c37b8327
jermp96 <jjeremyperez96@gmail.com>
2025-12-30 10:40:06 -0600

    add loadNextPage method to DiscoverProvider for loading video posts

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_vide_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // todo: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts.map(
+      (el) => LocalVideModel.fromJsonMap(el).toVideoPostEntity(),
+    ).toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
