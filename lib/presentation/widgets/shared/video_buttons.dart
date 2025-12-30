cb1677789dd019167d2e88de3d89e09b4b8491b6
jermp96 <jjeremyperez96@gmail.com>
2025-12-30 12:04:59 -0600

    add animate_do dependency and update VideoButtons for enhanced UI interactions

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/humman_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -15,9 +16,19 @@ class VideoButtons extends StatelessWidget {
           iconData: Icons.favorite,
           iconColor: Colors.red,
         ),
+        const SizedBox(height: 20,),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20,),
+        SpinPerfect(
+          infinite: true,
+          duration: Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outline,
+          ),
         ),
       ],
     );
@@ -43,6 +54,7 @@ class _CustomIconButton extends StatelessWidget {
           onPressed: () {},
           icon: Icon(iconData, color: color, size: 30),
         ),
+        if (value > 0)
         Text(HummanFormats.hummanReadleNumber(value.toDouble())),
       ],
     );
